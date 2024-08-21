120c27d734bd0b08d4ba7dadf3edbc39a9ea7ddd
siddhusathu20 <53175949+siddhusathu20@users.noreply.github.com>
2024-08-21 20:55:48 +0530

    Add files via upload

---
+++ Java Edition/jal/src/main/java/com/siddhusathu20/jal/Environment.java
@@ -0,0 +1,39 @@
+package com.siddhusathu20.jal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Environment {
+    final Environment enclosing;
+    final Map<String, Object> vars = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
+
+    void define(String name, Object value) {
+        vars.put(name, value);
+    }
+
+    void assign(Token name, Object value) {
+        if (vars.containsKey(name.lexeme)) {
+            vars.put(name.lexeme, value);
+            return;
+        }
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+        }
+        throw new Evaluator.RuntimeError(name, "Variable " + name.lexeme + " not defined");
+    }
+
+    Object get(Token name) {
+        if (vars.containsKey(name.lexeme))
+            return vars.get(name.lexeme);
+        if (enclosing != null) return enclosing.get(name);
+        throw new Evaluator.RuntimeError(name, "Variable " + name.lexeme + " not defined");
+    }
+}
